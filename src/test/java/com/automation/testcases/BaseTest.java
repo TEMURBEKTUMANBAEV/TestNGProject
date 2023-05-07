2f42e43a0de7b7b37e52856ba883307932f676e1
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-07 22:50:41 +0300

    test

--- src/test/java/com/automation/testcases/BaseTest.java
+++ src/test/java/com/automation/testcases/BaseTest.java
@@ -12,30 +12,50 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class BaseTest {
 
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void setUp() throws MalformedURLException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Where do you want to execute your test?");
+        String platform = sc.nextLine();
+        if (platform.equals("Local")){
+            driver = getLocalDriver();
+        } else {
+            driver = getLocalDriver();
+        }
+
         driver.get("https://www.google.com");
 
     }
 
-    public void getCloudDriver() throws MalformedURLException {
+    public WebDriver getCloudDriver() throws MalformedURLException {
         ChromeOptions browserOptions = new ChromeOptions();
         browserOptions.setPlatformName("Windows 11");
         browserOptions.setBrowserVersion("latest");
         Map<String, String> sauceOptions = new HashMap<>();
-        sauceOptions.put("build", "selenium-build-G3TT5");
+        sauceOptions.put("build", "MyBuild");
         sauceOptions.put("name", "Chrome Browser Parallel Execution");
         browserOptions.setCapability("sauce:options", sauceOptions);
-        URL url = new URL("https://oauth-seleniumtraining2015-e9cee:*****f698@ondemand.eu-central-1.saucelabs.com:443/wd/hub")
+        URL url = new URL("https://oauth-temurbektumanbaev638-96a26:2bf0e25f-2a26-40d2-9121-5d707ad2d11c@ondemand.eu-central-1.saucelabs.com:443/wd/hub");
         RemoteWebDriver driver = new RemoteWebDriver(url, browserOptions);
+        return driver;
     }
+
+    public WebDriver getLocalDriver(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
+
+
+
+
     @AfterMethod
     public void cleanUp(){
         System.out.println("Close browser");
