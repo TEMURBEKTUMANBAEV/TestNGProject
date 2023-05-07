f5de87d34ae4ebd394f22cb0aefca311cfb48af0
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-07 22:16:19 +0300

    Update BaseTest.java

--- src/test/java/com/automation/testcases/BaseTest.java
+++ src/test/java/com/automation/testcases/BaseTest.java
@@ -18,6 +18,17 @@ public class BaseTest {
 
     }
 
+    public void getCloudDriver(){
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 11");
+        browserOptions.setBrowserVersion("latest");
+        Map<String, String> sauceOptions = new HashMap<>();
+        sauceOptions.put("build", "selenium-build-G3TT5");
+        sauceOptions.put("name", "Chrome Browser Parallel Execution");
+        browserOptions.setCapability("sauce:options", sauceOptions);
+        URL url = new URL("https://oauth-seleniumtraining2015-e9cee:*****f698@ondemand.eu-central-1.saucelabs.com:443/wd/hub")
+        RemoteWebDriver driver = new RemoteWebDriver(url, browserOptions);
+    }
     @AfterMethod
     public void cleanUp(){
         System.out.println("Close browser");
