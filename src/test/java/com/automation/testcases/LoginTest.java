4907e3bbdf1483d27c42b5bbed85c09ceb5f2d5d
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-08 10:04:05 +0300

    Update LoginTest.java

--- src/test/java/com/automation/testcases/LoginTest.java
+++ src/test/java/com/automation/testcases/LoginTest.java
@@ -1,10 +1,12 @@
 package com.automation.testcases;
 
-import org.testng.annotations.*;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest{
 
     @Test(groups = {"SmokeTest", "ChiragTest"})
+    @Parameters("browser")
     public void verifyUserCanLogin(String browser) {
         System.out.println("verify login successful");
         System.out.println("Opening browser..." + browser);
