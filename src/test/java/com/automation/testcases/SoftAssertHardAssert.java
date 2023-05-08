21088282ae61fcca06a67c1d299a631accefbf66
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-08 09:13:17 +0300

    Update SoftAssertHardAssert.java

--- src/test/java/com/automation/testcases/SoftAssertHardAssert.java
+++ src/test/java/com/automation/testcases/SoftAssertHardAssert.java
@@ -2,17 +2,19 @@ package com.automation.testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class SoftAssertHardAssert {
 
     @Test
     public void testSoftAssert(){
-        Assert.assertEquals(2,2);
-        Assert.assertEquals("Chirag","Chriag");
-        Assert.assertEquals(10,10);
-        Assert.assertEquals("Test","Test");
-        Assert.assertEquals("Devx","Devx");
-        Assert.assertEquals(true,true);
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(2,2);
+        softAssert.assertEquals("Chirag","Chriag");
+        softAssert.assertEquals(10,10);
+        softAssert.assertEquals("Test","Test");
+        softAssert.assertEquals("Devx","Devx");
+        softAssert.assertEquals(true,true);
         System.out.println("Hard Assert Test Ends");
     }
 
