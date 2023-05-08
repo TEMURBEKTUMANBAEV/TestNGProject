cb54f0b7335197ecd1c8d222e9de0fa5cde44b4a
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-08 09:11:28 +0300

    test

--- src/test/java/com/automation/testcases/SoftAssertHardAssert.java
+++ src/test/java/com/automation/testcases/SoftAssertHardAssert.java
@@ -4,15 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SoftAssertHardAssert {
-    
+
     @Test
     public void testSoftAssert(){
-
+        Assert.assertEquals(2,2);
+        Assert.assertEquals("Chirag","Chriag");
+        Assert.assertEquals(10,10);
+        Assert.assertEquals("Test","Test");
+        Assert.assertEquals("Devx","Devx");
+        Assert.assertEquals(true,true);
+        System.out.println("Hard Assert Test Ends");
     }
+
     @Test
     public void testHardAssert(){
         Assert.assertEquals(2,2);
-        Assert.assertEquals("Chirag","Chirag");
+        Assert.assertEquals("Chirag","Chriag");
         Assert.assertEquals(10,10);
         Assert.assertEquals("Test","Test");
         Assert.assertEquals("Devx","Devx");
