445804df4b2c53d77edfacaf4ce1f9165bb9e6ac
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-08 11:03:59 +0300

    test

--- src/test/java/com/automation/testcases/DataDrivenTest.java
+++ src/test/java/com/automation/testcases/DataDrivenTest.java
@@ -2,15 +2,14 @@ package com.automation.testcases;
 
 import com.ebay.LoginFeatures;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataDrivenTest {
     LoginFeatures loginFeatures = new LoginFeatures();
 
-    @Test
-    public void verifyLoginUnsuccessfulWithInvalidCredentials(){
-        // Test Data
-       String username = "Chirag", password = "admin@123";
+    @Test (dataProvider = "invalidData")
+    public void verifyLoginUnsuccessfulWithInvalidCredentials(String username, String password){
 
        // Testing the Method with Test Data
        boolean result = loginFeatures.doLogin(username, password);
@@ -18,28 +17,21 @@ public class DataDrivenTest {
        // Checking the output
         Assert.assertEquals(result, false);
     }
-    @Test
-    public void verifyLoginUnsuccessfulWithInvalidCredentials2(){
-        // Test Data
-        String username = "admin", password = "admin@111";
 
-        // Testing the Method with Test Data
-        boolean result = loginFeatures.doLogin(username, password);
-
-        // Checking the output
-        Assert.assertEquals(result, false);
+    @DataProvider (name = "invalidData")
+    public  Object[][] getInvalidData(){
+        String[][] credentials = {
+                {"admin", "admin123"},
+                {"admin123", "admin123"},
+                {"chirag", "admin123"},
+                {"admin", "@123"},
+                {"devx", "devx@123"},
+                {"", ""},
+                {"", "admin123"},
+        };
+        return credentials;
     }
-    @Test
-    public void verifyLoginUnsuccessfulWithInvalidCredentials3(){
-        // Test Data
-        String username = "Admin", password = "admin@111";
 
-        // Testing the Method with Test Data
-        boolean result = loginFeatures.doLogin(username, password);
-
-        // Checking the output
-        Assert.assertEquals(result, false);
-    }
 
     @Test
     public void verifyLoginSuccessfulWithValidCred(){
