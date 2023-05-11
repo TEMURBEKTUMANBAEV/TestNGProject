1400ea96cff9ed95b0c7087ca2fc2ccfa7995365
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-11 22:28:27 +0300

    Update ReadDataFromExcel.java

--- src/test/java/com/automation/testcases/ReadDataFromExcel.java
+++ src/test/java/com/automation/testcases/ReadDataFromExcel.java
@@ -1,7 +1,10 @@
 package com.automation.testcases;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ReadDataFromExcel {
     public static void main(String[] args) {
-        
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
     }
 }
